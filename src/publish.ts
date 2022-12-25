1ded866f57aa8d8bd2fe805c7bd7fb84e9de7960
Emil <eminor992@gmail.com>
2022-12-25 21:46:25 +0100

    automatic refresh

--- src/publish.ts
+++ src/publish.ts
@@ -3,9 +3,14 @@ import fs from "fs";
 
 child_process.execSync('git add .');
 child_process.execSync('git commit -m "automatic refresh"');
+
+console.log("Publishing to master...");
 child_process.execSync('git push');
 
-fs.mkdirSync("dist/webpage/content");
+console.log("Publishing to gh-pages...");
+fs.mkdirSync("dist/webpage/content", {
+    recursive: true
+});
 fs.readdirSync("dist/content", "utf-8")
     .filter(filename => filename.endsWith("compressed"))
     .forEach(filename => fs.copyFileSync("dist/content/" + filename, "dist/webpage/content/" + filename));
